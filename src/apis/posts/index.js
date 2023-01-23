03191d457c02b2f3a0089e68649d89bb450406b9
Daniel <dan.earp@icloud.com>
2023-01-23 14:44:07 +0000

    Update index.js

--- src/apis/posts/index.js
+++ src/apis/posts/index.js
@@ -2,8 +2,20 @@ import express from 'express'
 import { model } from 'mongoose'
 import posts from './model.js'
 import createHttpError from 'http-errors'
+import { CloudinaryStorage } from 'multer-storage-cloudinary'
+import multer from 'multer'
+import { v2 as cloudinary } from 'cloudinary'
 
 const postsRouter = express.Router()
+
+const postCoverPhoto = multer({
+  storage: new CloudinaryStorage({
+    cloudinary,
+    params: {
+      folder: 'LinkedIn',
+    },
+  }),
+}).single('postPhoto')
 
 //Add a new post
 postsRouter.post('/', async (request, response, next) => {
@@ -79,5 +91,23 @@ postsRouter.delete('/:postID', async (request, response, next) => {
     next(err)
   }
 })
+
+//Upload an image and attach it to a post
+postsRouter.post(
+  '/:postID/uploadPostImage',
+  postCoverPhoto,
+  async (request, response, next) => {
+    try {
+      const url = request.file.path
+
+      const post = await posts.findByIdAndUpdate(request.params.postID, {
+        image: url,
+      })
+      response.status(200).send('Image uploaded')
+    } catch (err) {
+      next(err)
+    }
+  },
+)
 
 export default postsRouter
